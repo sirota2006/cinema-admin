03438d9811c1a7d7f029585d0eeb65f267d16d0b
sirota2006 <sirota200695@gmail.com>
2019-01-30 18:38:04 +0200

    build

--- src/app/apis/ombapi.ts
+++ src/app/apis/ombapi.ts
@@ -1,5 +1,5 @@
 import axios from "axios"
 
 export default axios.create({
-  baseURL: 'http://www.omdbapi.com/?i=tt3896198&apikey=61c5fbb7&t=',
+  baseURL: 'https://www.omdbapi.com/?i=tt3896198&apikey=61c5fbb7&t=',
 });
